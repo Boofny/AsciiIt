45fb58340ab90cc869103a1d73aaf5477aeb69ee
Boofny <Brandonpizano7557@gmail.com>
2026-02-19 10:46:42 -0500

    found how to upload files in the frontend but will need to work on a multi stepp validations for the file uploads

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,6 +3,9 @@ function App() {
   return (
     <>
       <p className="text-red-600">slime</p>
+      <button type="submit">Send file</button>
+      <br />
+      <input className="bg-red-300 cursor-pointer hover:bg-red-500" type="file" name="" id=""/>
     </>
   )
 }
